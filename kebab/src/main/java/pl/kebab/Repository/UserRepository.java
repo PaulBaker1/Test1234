ba0f13a1fe7e34c86edabf5d8dcfe39e1cc58990
kamil <kamilsajdok@wp.pl>
2021-06-19 14:23:13 +0200

    bugfixes

--- kebab/src/main/java/pl/kebab/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.kebab.Repository;
-
-import pl.kebab.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends
-        CrudRepository<User, Long> {}
